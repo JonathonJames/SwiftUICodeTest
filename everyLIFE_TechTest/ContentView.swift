353bd954922470ac09d25e5b55d917b5f0010420
Jonathon James <jonathonjames@gmail.com>
2021-08-31 10:27:27 +0100

    Initial Commit

---
+++ everyLIFE_TechTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  everyLIFE_TechTest
+//
+//  Created by Uncreation on 31/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ everyLIFE_TechTest/everyLIFE_TechTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  everyLIFE_TechTestApp.swift
+//  everyLIFE_TechTest
+//
+//  Created by Uncreation on 31/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct everyLIFE_TechTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
